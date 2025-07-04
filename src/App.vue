20913d736f2eef9c39e50e9f65367a9e043351e5
zhamanko <slavko.lenko28.06.2016@gmail.com>
2025-07-04 14:46:40 +0300

    feat: add Messange component and integrate it into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import SlideBarLeft from "@/components/SlideBarLeft.vue";
+import Messange from "./components/Messange.vue";
 </script>
 
 <template>
   <SlideBarLeft/>
   <router-view class="flex-1"></router-view>
+    <Messange/>
 </template>
---
+++ src/components/Messange.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="absolute bottom-3 text-center left-1/2 right-1/2 bg-[#1a1a1a] text-white p-4 rounded-3xl shadow-lg w-1/2 transform -translate-x-1/2">
+        <h1>Повідомлення</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+</template>
