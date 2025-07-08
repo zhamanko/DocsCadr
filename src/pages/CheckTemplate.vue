f9d27a3fddddd737b3bcaf27c22f715889430e5d
zhamanko <slavko.lenko28.06.2016@gmail.com>
2025-07-08 10:32:28 +0300

    feat: update CheckTemplate; display selected file name dynamically

--- src/pages/CheckTemplate.vue
+++ src/pages/CheckTemplate.vue
@@ -9,12 +9,13 @@ export default {
     data() {
         return {
             file: null,
+            fileName: "",
         };
     },
     methods: {
         onFileSelected(file){
             this.file = file;
-            console.log(this.file);
+            this.fileName = file.name;
         }
     }
 };
@@ -27,7 +28,7 @@ export default {
                 <!-- <input type="file" name="templateFile" id="templateFile" class="bg-[#23262b] w-full mb-4 text-white rounded-3xl file:px-4 file:py-2 file:bg-[#1c1e20] hover:bg-[#2d3036] hover:scale-101 focus:bg-[#2d3036] focus:scale-101 transition" accept=".docx, .doc, .pdf">
                 <button class="bg-[#263028] w-full text-white rounded-3xl p-2 hover:bg-[#2e3d31] focus:bg-[#2e3d31] hover:scale-101 focus:scale-101 transition">Завантажити</button> -->
                 <ComponentUploadFile @file-selected="onFileSelected"/>
-                <p class="mt-6 mx-2">Назва файла: {{ this.file.name }}</p>
+                <p class="mt-6 mx-2">Назва файла: {{ this.fileName }}</p>
             </div>
         </div>
         <div class="flex flex-row gap-2">
