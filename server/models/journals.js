e08941c15705b642cc7229c6fca9fb19da02a039
zhamanko <slavko.lenko28.06.2016@gmail.com>
2025-07-23 13:51:07 +0300

    feat: add letters_num column to journals table for improved data tracking

--- server/models/journals.js
+++ server/models/journals.js
@@ -6,6 +6,7 @@ function initJournals() {
       CREATE TABLE IF NOT EXISTS journals (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         number TEXT NOT NULL,
+        letters_num TEXT NOT NULL,
         name TEXT NOT NULL,
         date TEXT NOT NULL,
         file TEXT NOT NULL,
